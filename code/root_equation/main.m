9a31d369b3bc5f29737c6b8c6ef31689dc39c4fe
MayReel <krachu2904@gmail.com>
2025-01-17 02:56:56 +0700

    Add secant method

--- code/root_equation/main.m
+++ code/root_equation/main.m
@@ -4,6 +4,8 @@ disp('1. test Newton Raphson Solution');
 disp('2. test Bisection Method Solution');
 disp('3. test False-Position Method Solution');
 disp('4. test Increment Search Solution');
+disp('5. test Secant Method Solution');
+disp('6. test Secant Method Solution (perturbation)');
 f = input('Enter number function : ');
 
 switch f
@@ -15,6 +17,10 @@ switch f
         false_position_general();
     case 4
         increment_search();
+    case 5
+        secant_general();
+    case 6
+        secant_perturbation();
     otherwise
         disp('LLLLLLLLLLLLLLLL');
 end
